b412d96f6f6d8391d092a5bf84a5a6f21b1f04fd
AllenWish <1697591868@qq.com>
2018-06-26 22:04:43 +0800

    读写分离

--- src/main/java/com/allentest/config/DataSourceConfig.java
+++ src/main/java/com/allentest/config/DataSourceConfig.java
@@ -10,20 +10,23 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.sql.DataSource;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
-@Configuration
+//@Configuration
 public class DataSourceConfig {
     @Bean
-    public DataSource getDataSource() throws SQLException {
+    public DataSource getDataSource() throws SQLException, IOException {
         return buildDataSource();
     }
 
-    private DataSource buildDataSource() throws SQLException {
+    private DataSource buildDataSource() throws SQLException, IOException {
         // 构建读写分离数据源, 读写分离数据源实现了DataSource接口, 可直接当做数据源处理. masterDataSource0, slaveDataSource00, slaveDataSource01等为使用DBCP等连接池配置的真实数据源
-        Map<String, DataSource> dataSourceMap = new HashMap<>();
+        /*Map<String, DataSource> dataSourceMap = new HashMap<>();
         dataSourceMap.put("shjd-test-0", createDataSource("shjd-test"));
         dataSourceMap.put("shjd-test-1", createDataSource("shjd-test-1"));
         dataSourceMap.put("shjd-test-2", createDataSource("shjd-test-2"));
@@ -36,8 +39,11 @@ public class DataSourceConfig {
         //这里数据库从库配置两个，查询的时候会负载均衡去匹配一个数据去查询
         masterSlaveRuleConfig0.getSlaveDataSourceNames().add("shjd-test-1");
         masterSlaveRuleConfig0.getSlaveDataSourceNames().add("shjd-test-2");
-
-        DataSource dataSource = MasterSlaveDataSourceFactory.createDataSource(dataSourceMap, masterSlaveRuleConfig0,new HashMap<>());
+*/
+        //DataSource dataSource = MasterSlaveDataSourceFactory.createDataSource(dataSourceMap, masterSlaveRuleConfig0,new HashMap<>());
+        URL resource = getClass().getResource("/application.properties");
+        File file = new File(resource.getFile());
+        DataSource dataSource = MasterSlaveDataSourceFactory.createDataSource(file);
 
         return dataSource;
     }
